f2d5fd466dd3928d54d1b6e7f8d9e5ff83142a65
Icaro Assis <icaroassisfreitas@gmail.com>
2022-01-31 15:51:50 -0300

    cria componente de header na main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,15 @@
-import './styles.css'
+import * as S from './styles'
 import React from 'react'
+import Header from './Header'
 
 function Main() {
   return (
-    <div>
+    <S.Wrapper>
+      <Header />
+      <div>Shop</div>
       <h1>Main</h1>
-    </div>
+      <h1>Produtos</h1>
+    </S.Wrapper>
   )
 }
 
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  background-color: #f2f2f2;
+  flex-direction: column;
+  height: 100vh;
+`
