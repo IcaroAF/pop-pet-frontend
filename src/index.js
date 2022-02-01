9f202dc73a5480c581cde973841012febea8529d
Icaro Assis <icaroassisfreitas@gmail.com>
2022-01-31 21:31:28 -0300

    cria contexto do carrinho

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom'
 import Routes from './routes'
 import GlobalStyles from './styles/global'
 import { ChakraProvider } from '@chakra-ui/react'
+import { CartContextProvider } from './contexts/cartContext'
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <GlobalStyles />
-      <Routes />
+      <CartContextProvider>
+        <GlobalStyles />
+        <Routes />
+      </CartContextProvider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
