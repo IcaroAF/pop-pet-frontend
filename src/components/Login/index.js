3ff85aedcc305cbba473faeac5831ceaf77d5e44
Icaro Assis <icaroassisfreitas@gmail.com>
2022-02-02 10:44:04 -0300

    atualiza componente de login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 import {
   Button,
   Popover,
@@ -12,44 +14,85 @@ import {
   Portal,
 } from '@chakra-ui/react'
 import { useForm } from 'react-hook-form'
-import { LoginButton, LoginInput } from './styles'
+import { LoginButton, LoginInput, LogoutButton } from './styles'
+import { AuthContext } from '../../contexts/authContext'
+import { UserContext } from '../../contexts/userContext'
 
 function Login() {
+  const { token, setToken } = useContext(AuthContext)
+  const { setUser } = useContext(UserContext)
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios({
+        url: 'https://pop-pet-challenge.herokuapp.com/login',
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        data: data,
+      })
+
+      if (response.status === 200) {
+        setToken(response.data.token)
+        setUser(response.data.userLogged)
+        localStorage.setItem('token', response.data.token)
+      }
+    } catch (error) {
+      return error.message
+    }
+  }
   const { register, handleSubmit } = useForm()
+
+  function logout() {
+    setToken('')
+    localStorage.removeItem('token')
+  }
+
   return (
-    <Popover>
-      <PopoverTrigger color="blackAlpha">
-        <LoginButton colorScheme="blackAlpha" color="white">
-          Entrar
-        </LoginButton>
-      </PopoverTrigger>
-      <Portal>
-        <PopoverContent>
-          <PopoverArrow />
-          <PopoverHeader fontSize="lg">Faça seu Login</PopoverHeader>
-          <PopoverCloseButton />
-          <PopoverBody size="20">
-            <form onSubmit={handleSubmit()}>
-              <LoginInput
-                type="text"
-                id="email"
-                placeholder="seuemail@dominio.com"
-                {...register('email', { required: true })}
-              />
-              <LoginInput
-                type="password"
-                id="password"
-                {...register('password', { required: true })}
-              />
-              <Button type="submit">Login </Button>
-            </form>
-          </PopoverBody>
-          <PopoverFooter fontSize="lg">
-            Ainda não tem uma conta? Cadastre-se
-          </PopoverFooter>
-        </PopoverContent>
-      </Portal>
-    </Popover>
+    <>
+      {token ? (
+        <LogoutButton to="/" onClick={logout}>
+          <span>Deslogar</span>
+        </LogoutButton>
+      ) : (
+        <Popover>
+          <PopoverTrigger color="blackAlpha">
+            <LoginButton colorScheme="blackAlpha" color="white">
+              Entrar
+            </LoginButton>
+          </PopoverTrigger>
+          <Portal>
+            <PopoverContent>
+              <PopoverArrow />
+              <PopoverHeader fontSize="lg">Faça seu Login</PopoverHeader>
+              <PopoverCloseButton />
+              <PopoverBody size="20">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <LoginInput
+                    type="text"
+                    id="email"
+                    placeholder="seuemail@dominio.com"
+                    {...register('email', { required: true })}
+                  />
+                  <LoginInput
+                    type="password"
+                    id="password"
+                    {...register('password', { required: true })}
+                  />
+                  <Button type="submit">Login </Button>
+                </form>
+              </PopoverBody>
+              <PopoverFooter fontSize="lg">
+                Ainda não tem uma conta? <Link to="/sign-up">Cadastre-se</Link>
+              </PopoverFooter>
+            </PopoverContent>
+          </Portal>
+        </Popover>
+      )}
+    </>
   )
 }
 
